f3d661e515103321083f2ced1471f3526e57db44
Firly Hafiz Syahreza <firlyhafizsyahreza42@gmail.com>
2025-11-05 09:06:11 +0700

    fix (models): changing AlurPelayaran to be appended in the models list

--- apps/maps/static/static_base/models/__init__.py
+++ apps/maps/static/static_base/models/__init__.py
@@ -4,6 +4,7 @@ from .models import *
 from .gis_models import *
 
 # You can also add __all__ to define the public API of the package
-__all__ = ['ZipFile', 'Coords', 'AlurPelayaran', 'ZipFileForm', 'CoordsForm']
+__all__ = ['ZipFile', 'Coords', 'ZipFileForm', 'CoordsForm']
+__all__.append('AlurPelayaran')
 __all__.append("KabelBawahLaut")
 __all__.append("Migas")
